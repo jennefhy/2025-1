5b35217472057760587e43fa1a18248f9842bbf7
Jennefhy Saiury <saiuryjennefhy5@gmail.com>
2025-06-09 13:08:02 -0300

    Update questao1.c

---
+++ complexidade/questao1.c
@@ -0,0 +1,6 @@
+//O algoritmo serve para medir o tempo medio que uma funcao recursiva demora para rodar com um valor fixo de entrada. 
+//Ele executa essa funcao varias vezes, anota o tempo de cada execucao, soma todos esses tempos e depois divide pelo 
+//número total de execucoes para obter a media. 
+//O tempo total que o algoritmo leva depende de quantas vezes ele repete a funcao e do tempo que a funcao demora para rodar uma unica vez. 
+//Se a funcao recursiva for lenta, o tempo total sera maior, porque o algoritmo repete essa execucao varias vezes para calcular a media. 
+//Portanto, o tempo total é proporcional ao tempo da funcao vezes o numero de repeticoes
