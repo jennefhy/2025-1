40950034530c69ea59db5944f7700b847d5869ac
Jennefhy Saiury <saiuryjennefhy5@gmail.com>
2025-06-09 09:39:07 -0300

    Update questao2.c
    função main para entrada de base e expoente e exibe resultado da potência

--- exercicio/questao2.c
+++ exercicio/questao2.c
@@ -16,3 +16,21 @@ int potencia(int base, int expoente){
     return resul;   
 }
 
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int base, expoente;
+
+    printf("Digite a base: ");
+    scanf("%d", &base);
+    printf("Digite o expoente positivo: ");
+    scanf("%d", &expoente);
+
+    int resul = potencia(base, expoente);
+
+    if (resul != -1)
+    {
+        printf("%d elevado a %d = %d\n", base, expoente, resul);
+    }
+    return 0;
+}
