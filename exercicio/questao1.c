135e3172ce19c190f74e29a01f9bbb8d8f53c27b
Jennefhy Saiury <saiuryjennefhy5@gmail.com>
2025-06-09 09:21:41 -0300

    update
    adiciona função main para ler intervalo e listar números primos usando a função ehPrimo

--- exercicio/questao1.c
+++ exercicio/questao1.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-bool primo(int n){
+bool ehPrimo(int n){
     if (n <= 1) return false;
     if (n == 2) return true;
     if (n % 2 == 0) return false;
@@ -13,4 +13,31 @@ bool primo(int n){
         return false;
     }
     return true;
+}
+
+int main(){
+    int num1, num2;
+
+    printf("Digite um número inicial para o intervalo: ");
+    scanf("%d", &num1);
+    printf("Digite um número final para o intervalo: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2){
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    printf("Números primos entre %d e %d:\n", num1, num2);
+
+    for (int i = num1; i <= num2; i++){
+        if (ehPrimo(i))
+        {
+            printf("%d", i);
+        }
+    }
+    printf("\n");
+    
+    return 0;
 }
